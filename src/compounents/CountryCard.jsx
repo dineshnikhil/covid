4b48a984102430842b032e52b6b214d47ec6cce4
DineshNikhil <dineshnikhil@pop-os.localdomain>
2022-01-15 08:22:55 +0530

    changed the input field position home file.

--- src/compounents/CountryCard.jsx
+++ src/compounents/CountryCard.jsx
@@ -6,7 +6,7 @@ function CountryCard(props) {
             <h1 className='text-secondary text-2xl font-firstfont font-semibold mb-4'>{props.data.Country}</h1>
             <p>TotalConfirmed : {props.data.TotalConfirmed}</p>
             <p className='mb-4'>NewConfirmed : {props.data.NewConfirmed}</p>
-            <a href="#" className='bg-shadecolor text-bgcolor text-lg px-6 py-2 rounded-md my-6 hover:bg-green-800'>Know More</a>
+            {/* <a href="#" className='bg-shadecolor text-bgcolor text-lg px-6 py-2 rounded-md my-6 hover:bg-green-800'>Know More</a> */}
         </div>
     )
 }
--- src/compounents/Home.jsx
+++ src/compounents/Home.jsx
@@ -32,12 +32,7 @@ function Home() {
 
 
     return (
-        <div>
-            <div className='py-4'>
-                <input className='bg-green-600 border-2 border-green-400 text-stone-300 py-2 px-8 rounded-md' type="text" placeholder='search your country' onChange={e => {
-                    setsearch(e.target.value)
-                }}/>
-            </div>
+        <div className='py-8'>
             <div className='bg-shadecolor w-3/4 py-8 px-6 mx-auto rounded-md mb-8 drop-shadow-lg'>
                 <h1 className='text-secondary text-2xl md:text-3xl font-firstfont font-semibold'>Global Covid Information</h1>
                 <div className='grid grid-cols-1 md:grid-cols-2 my-8'>
@@ -47,6 +42,11 @@ function Home() {
                     <h1 className='text-black-xl text-lg md:text-2xl'>TotalDeaths : {totalDeaths}</h1>
                 </div>
                 <a href="https://www.google.com/search?q=covid&oq=covid&aqs=chrome..69i57j69i59j0i131i433i512l2j0i131i433i457i512j69i60j69i61j69i60.1538j0j7&sourceid=chrome&ie=UTF-88" target="_blank" className='py-4 px-8 text-md text-bgcolor bg-secondary my-8 rounded-xl'>Know More</a>
+            </div>
+            <div className='py-4'>
+                <input className='bg-green-600 border-2 border-green-400 text-stone-300 py-2 px-8 rounded-md' type="text" placeholder='search your country' onChange={e => {
+                    setsearch(e.target.value)
+                }}/>
             </div>
             <div className='py-4 px-8 grid grid-cols-1 xl:grid-cols-4 gap-4 md:grid-cols-3 2xl:grid-cols-6'>
                 {isLoading && <h1>loading...</h1>}
